072e43c87fc950823bbec735cc0f3db726d7c89c
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-06 18:30:40 -0300

    Atualização de desafios, fases de resolução

---
+++ PrimeirosDesafiosComC#/QuantNumPos.cs
@@ -0,0 +1,20 @@
+using System;
+//AINDA EM FASE DE RESOLUÇÃO
+//AINDA EM FASE DE RESOLUÇÃO
+//AINDA EM FASE DE RESOLUÇÃO
+//AINDA EM FASE DE RESOLUÇÃO
+//AINDA EM FASE DE RESOLUÇÃO
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		int quantidadePositivos = 0;
+		for (int i=0; i < 6; i++) 
+		{
+			double valorInformadoPeloUsuario = double.Parse(Console.ReadLine());
+			// TODO Incrementar a "quantidadePositivos" somente SE o "valorInformadoPeloUsuario" for positivo.
+
+		}
+		Console.WriteLine(quantidadePositivos + " valores positivos");
+	}
+}
