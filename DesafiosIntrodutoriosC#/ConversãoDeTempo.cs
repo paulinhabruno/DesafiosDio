ca1176328a32feee3e876c39e13481a88ad2e4f7
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-06 17:08:21 -0300

    Atualização de desafios

---
+++ DesafiosIntrodutoriosC#/ConversãoDeTempo.cs
@@ -0,0 +1,14 @@
+using System;
+
+    class MinhaClasse {
+        static void Main(string[] args) {
+            var timeInSeconds = int.Parse(Console.ReadLine());
+
+       //continue a solucao
+            var hours = (timeInSeconds/3600);
+            var minutes = (timeInSeconds % 3600) / 60  ;
+            var seconds =    (timeInSeconds % 3600) % 60;
+
+            Console.WriteLine($"{hours}:{minutes}:{seconds}");
+        }
+    }
