45c6a77ad360d5003abeb238bb162c97bb8a2d0e
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-07 18:05:47 -0300

    atualização de desafios

---
+++ DesafiosBasicosC#/ParOuImpar.cs
@@ -0,0 +1,49 @@
+using System; 
+// EM FASE DE RESOLUÇÃO
+// EM FASE DE RESOLUÇÃO
+// EM FASE DE RESOLUÇÃO
+// EM FASE DE RESOLUÇÃO
+class Desafio {
+
+        public static void Main()
+        {
+
+            int tc, i, x;
+            tc = int.Parse(Console.ReadLine());
+            for ( int i = 0 ; i < tc ; i++ )
+            {
+                x = int.Parse(Console.ReadLine());
+                if (  x == 0   )
+                {
+                    Console.WriteLine("NULL");
+                }
+                else if (    )
+                {
+                    if (   )
+                    {
+                        Console.WriteLine("EVEN POSITIVE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("EVEN NEGATIVE");
+                    }
+
+                }
+                else
+                {
+                    if (   )
+                    {
+                        Console.WriteLine("ODD POSITIVE");
+                    }
+                    else
+                    {
+                        Console.WriteLine("ODD NEGATIVE");
+                    }
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+
+}
