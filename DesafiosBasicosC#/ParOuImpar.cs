2912b5d69135d3c0a54faf79aa9cec72ec018cd2
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-09 16:55:04 -0300

    Desafios executados

--- DesafiosBasicosC#/ParOuImpar.cs
+++ DesafiosBasicosC#/ParOuImpar.cs
@@ -1,49 +1,23 @@
 using System; 
-// EM FASE DE RESOLUÇÃO
-// EM FASE DE RESOLUÇÃO
-// EM FASE DE RESOLUÇÃO
-// EM FASE DE RESOLUÇÃO
-class Desafio {
 
-        public static void Main()
-        {
-
-            int tc, i, x;
-            tc = int.Parse(Console.ReadLine());
-            for ( int i = 0 ; i < tc ; i++ )
-            {
-                x = int.Parse(Console.ReadLine());
-                if (  x == 0   )
-                {
-                    Console.WriteLine("NULL");
-                }
-                else if (    )
-                {
-                    if (   )
-                    {
-                        Console.WriteLine("EVEN POSITIVE");
-                    }
-                    else
-                    {
-                        Console.WriteLine("EVEN NEGATIVE");
-                    }
-
-                }
-                else
-                {
-                    if (   )
-                    {
-                        Console.WriteLine("ODD POSITIVE");
-                    }
-                    else
-                    {
-                        Console.WriteLine("ODD NEGATIVE");
-                    }
-                }
-
-            }
-
-            Console.ReadLine();
+class DesafioParOuImpar {
+  public static void Main() {
+    int tc, i, x;
+    
+    tc = int.Parse(Console.ReadLine());
+    
+    for ( i = 0; i < tc; i++) {
+      x = int.Parse(Console.ReadLine());
+      
+      if (x == 0) Console.WriteLine("NULL");
+      if (x != 0) {
+        if (x > 0) {
+          Console.WriteLine(x % 2 ==0 ? "EVEN POSITIVE" : "ODD POSITIVE");
+        } else if (x < 0) {
+          Console.WriteLine(x % 2 ==0 ? "EVEN NEGATIVE" : "ODD NEGATIVE");
         }
-
+      }
+    }
+    Console.ReadLine();
+  }
 }
