8170a79f9dab79086f27cf5b84a2f3d6701c5c1d
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-12 21:02:18 -0300

    Atualização de códigos

---
+++ DesafiosIniciaisEmC#/HoHoHo.cs
@@ -0,0 +1,31 @@
+// Papai Noel está brincando com seus duendes para entretê-los durante a véspera do Natal. A brincadeira consiste nos elfos escreverem números em pedaços de papel e colocarem no gorro do Papai Noel. Após todos terminarem de colocar os números Noel sorteia um papel e aquele número representa quantos "Ho" o Noel deve falar.
+
+//Seu trabalho é ajudar o Papai Noel montando um problema que mostre todos os "Ho" que ele deve falar dado o número sorteado.
+
+//Entrada
+//A entrada é composta por um único inteiro N (0 < N ≤ 106) representando quantos "Ho" serão falados por Noel.
+
+//Saída
+//A saída é composta por todos "Ho" que Papai Noel deve falar separados por um espaço. Após o último "Ho" deve ser apresentado um "!" encerrando o programa.
+
+
+using System;
+
+namespace _52._36_URI_1759 {
+    class Program {
+        static void Main(string[] args) {
+           
+            int N = int.Parse(Console.ReadLine());
+
+            //Exibir "Ho" do papai noel
+            for (int i = 1; i < N; i++) 
+            
+            {
+
+            Console.Write("Ho ");    
+            }
+
+            Console.Write("Ho!\n");
+        }
+    }
+}
