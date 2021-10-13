aeffff9421dc28b5ec2497d4d921ab2d43f3eab7
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-13 14:59:34 -0300

    Atualização de desafios

---
+++ DesafiosEmC#/Animal.cs
@@ -0,0 +1,58 @@
+//DESAFIO EM FASE DE CONCLUSÃO
+//DESAFIO EM FASE DE CONCLUSÃO
+//DESAFIO EM FASE DE CONCLUSÃO
+//DESAFIO EM FASE DE CONCLUSÃO
+
+
+//Neste problema, você deverá ler 3 palavras que definem o tipo de animal possível segundo o esquema abaixo, da esquerda para a direita.  Em seguida conclua qual dos animais seguintes foi escolhido, através das três palavras fornecidas.
+
+//Entrada
+//A entrada contém 3 palavras, uma em cada linha, necessárias para identificar o animal segundo a figura acima, com todas as letras minúsculas.
+
+//Saída
+//Imprima o nome do animal correspondente à entrada fornecida.
+
+using System;
+using System.Text;
+
+class MainClass {
+  public static void Main (string[] args) {
+        String  ; //declare as suas variaveis
+        String  ;
+        String  ;
+
+         = Console.ReadLine(); //insira suas variaveis
+         = Console.ReadLine();
+         = Console.ReadLine();
+
+      if (( ) && ( )  && ( )) {
+      //complete o desafio
+    }
+
+     if (( ) && ( )  && ( )) {
+      //complete o desafio
+    }
+
+     if (( ) && ( )  && ( )) {
+      //complete o desafio
+    }
+
+     if (( ) && ( )  && ( )) {
+      //complete o desafio
+    }
+
+     if (( ) && ( )  && ( )) {
+      //complete o desafio
+    }
+
+     if (( ) && ( )  && ( )) {
+      //complete o desafio
+    }
+
+    if ((  == "invertebrado") && (  == "anelideo") && ( == "onivoro")) {
+      Console.WriteLine("sanguessuga\n");
+    }
+
+
+  }
+}
--- PrimeirosDesafiosComC#/QuantNumPos.cs
+++ PrimeirosDesafiosComC#/QuantNumPos.cs
@@ -1,9 +1,13 @@
+//Desafio
+//Crie um programa que leia 6 valores, os quais poderão ser negativos e/ou positivos. Em seguida, apresente a quantidade de valores positivos digitados.
+
+//Entrada
+//Você receberá seis valores, negativos e/ou positivos.
+
+//Saída
+//Exiba uma mensagem dizendo quantos valores positivos foram lidos. assim como é exibido abaixo no exemplo de saída. Não se esqueça de incluir na mensagem de saída o sufixo " valores positivos", conforme o exemplo abaixo:
+
 using System;
-//AINDA EM FASE DE RESOLUÇÃO
-//AINDA EM FASE DE RESOLUÇÃO
-//AINDA EM FASE DE RESOLUÇÃO
-//AINDA EM FASE DE RESOLUÇÃO
-//AINDA EM FASE DE RESOLUÇÃO
 
 public class Program
 {
@@ -13,6 +17,10 @@ public class Program
 		for (int i=0; i < 6; i++) 
 		{
 			double valorInformadoPeloUsuario = double.Parse(Console.ReadLine());
+			
+			if ( valorInformadoPeloUsuario > 0) quantidadePositivos++;
+
+
 			// TODO Incrementar a "quantidadePositivos" somente SE o "valorInformadoPeloUsuario" for positivo.
 
 		}
