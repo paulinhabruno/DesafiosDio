326a3e2c30637d678f4ad2e18a2c911142f6927a
PAULA REGINA CARDOSO BRUNO <79319807+paulinhabruno@users.noreply.github.com>
2021-10-22 20:37:59 -0300

    Atualização de desafios

--- DesafiosEmC#/Animal.cs
+++ DesafiosEmC#/Animal.cs
@@ -17,40 +17,49 @@ using System.Text;
 
 class MainClass {
   public static void Main (string[] args) {
-        String palavra1 = Console.ReadLine();  //declare as suas variaveis
-        String  palavra2 = Console.ReadLine(); 
-        String  palavra3 = Console.ReadLine(); 
+        string palavra1 = Console.ReadLine();
+        string palavra2 = Console.ReadLine();
+        string palavra3 = Console.ReadLine();
 
-         
-
-      if (( ) && ( )  && ( )) {
-      //complete o desafio
+    if (( palavra1 == "vertebrado") && ( palavra2 == "ave") && ( palavra3 == "onivoro")) {
+      Console.WriteLine("pomba\n");    
+      
     }
+      
+      if (( palavra1 == "vertebrado") && ( palavra2 == "ave") && ( palavra3 == "carnivoro")) {
+      Console.WriteLine("aguia\n");    
+        
+      }
+      
+      if (( palavra1 == "vertebrado") && ( palavra2 == "mamifero") && ( palavra3 == "onivoro")) {
+      Console.WriteLine("homem\n");    
+        
+      }
+      
+      if (( palavra1 == "vertebrado") && ( palavra2 == "mamifero") && ( palavra3 == "herbivoro")) {
+      Console.WriteLine("vaca\n");    
+        
+      }
+      
+      if (( palavra1 == "invertebrado") && ( palavra2 == "inseto") && ( palavra3 == "hematofago")) {
+      Console.WriteLine("pulga\n");    
+      
+      }
+      
+      if (( palavra1 == "invertebrado") && ( palavra2 == "inseto") && ( palavra3 == "herbivoro")) {
+      Console.WriteLine("lagarta\n");    
+      
+      }
+      if (( palavra1 == "invertebrado") && ( palavra2 == "anelideo") && ( palavra3 == "hematofago")) {
+      Console.WriteLine("sanguessuga\n");    
+      
+      }
+      if (( palavra1 == "invertebrado") && ( palavra2 == "anelideo") && ( palavra3 == "onivoro")) {
+      Console.WriteLine("minhoca\n");    
+      
+      }
 
-     if (( ) && ( )  && ( )) {
-      //complete o desafio
-    }
-
-     if (( ) && ( )  && ( )) {
-      //complete o desafio
-    }
-
-     if (( ) && ( )  && ( )) {
-      //complete o desafio
-    }
-
-     if (( ) && ( )  && ( )) {
-      //complete o desafio
-    }
-
-     if (( ) && ( )  && ( )) {
-      //complete o desafio
-    }
-
-    if ((  == "invertebrado") && (  == "anelideo") && ( == "onivoro")) {
-      Console.WriteLine("sanguessuga\n");
-    }
-
-
+     
+    
   }
 }
